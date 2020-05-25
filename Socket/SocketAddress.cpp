a943f87412b85f73a7ba856189909489855c23cf
Sergey Miroshnichenko <ommmirage@gmail.com>
2020-05-25 13:09:46 +0400

    class SocketAddress

--- Socket/SocketAddress.cpp
+++ Socket/SocketAddress.cpp
@@ -1,1 +1,25 @@
 #include "SocketAddress.h"
+
+SocketAddress::SocketAddress(uint32_t inAddress, std::uint16_t inPort)
+{
+	GetAsSockAddrIn()->sin_family = AF_INET;
+	GetAsSockAddrIn()->sin_addr.S_un.S_addr = htonl(inAddress);
+	GetAsSockAddrIn()->sin_port = htons(inPort);
+}
+
+SocketAddress::SocketAddress(const sockaddr& inSockAddr)
+{
+	memcpy(&mSockAddr, &inSockAddr, sizeof(sockaddr));
+}
+
+size_t SocketAddress::GetSize() const
+{
+	return sizeof(sockaddr);
+}
+
+sockaddr_in* SocketAddress::GetAsSockAddrIn()
+{
+	return reinterpret_cast<sockaddr_in*>(&mSockAddr);
+}
+
+typedef shared_ptr<SocketAddress> SocketAddressPtr;
